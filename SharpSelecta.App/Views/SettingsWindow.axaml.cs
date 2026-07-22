2c5c93788147e1b1761939a8123b194e5498348e
ghorshy <ghorshy@protonmail.com>
2026-07-22 21:56:51 +0200

    Add a standard OK button to the Settings window

--- SharpSelecta.App/Views/SettingsWindow.axaml.cs
+++ SharpSelecta.App/Views/SettingsWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using CommunityToolkit.Mvvm.Input;
 using SharpSelecta.App.ViewModels;
 
 namespace SharpSelecta.App.Views;
@@ -8,6 +10,19 @@ public partial class SettingsWindow : Window
     public SettingsWindow()
     {
         InitializeComponent();
+    }
+
+    private async void OnOkClick(object? sender, RoutedEventArgs e)
+    {
+        if (DataContext is not SettingsWindowViewModel { SelectedCategoryViewModel: var category })
+            return;
+
+        if (category.ApplyCommand is IAsyncRelayCommand applyAsync)
+            await applyAsync.ExecuteAsync(null);
+        else
+            category.ApplyCommand.Execute(null);
+
+        Close();
     }
 
     private void OnClosing(object? sender, WindowClosingEventArgs e)
