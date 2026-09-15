c4c8fe790709e9c53b7771ecb267ddb0680c7557
ghorshy <ghorshy@protonmail.com>
2026-09-15 21:49:32 +0200

    Wire the waveform slider toggle into the playback toolbar

--- SharpSelecta.App/Resources/Strings.cs
+++ SharpSelecta.App/Resources/Strings.cs
@@ -44,6 +44,7 @@ public static class Strings
     public static string OutputDevice => Get(nameof(OutputDevice));
     public static string SystemDefaultAudioDevice => Get(nameof(SystemDefaultAudioDevice));
     public static string UseLogarithmicVolumeScale => Get(nameof(UseLogarithmicVolumeScale));
+    public static string UseWaveformSlider => Get(nameof(UseWaveformSlider));
     public static string NoLibraryFoldersAdded => Get(nameof(NoLibraryFoldersAdded));
     public static string Ok => Get(nameof(Ok));
     public static string Apply => Get(nameof(Apply));
